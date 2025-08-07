a175dce22f01ec1ab0dba05cedcb8a78cc554f2b
rahulyadav123215 <ry123215@gmail.com>
2025-08-07 08:06:17 +0530

    Day 6: Added Binary Search implementation

---
+++ Day06/BinarySearch/basics.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int search(vector<int> &nums, int target)
+{
+    int n = nums.size();
+    int low = 0, high = n - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (nums[mid] == target)
+            return mid;
+        else if (target > nums[mid])
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return -1;
+}
+
+int main()
+{
+    vector<int> nums = {3, 4, 6, 7, 9, 12, 16, 17};
+    int target = 13;
+
+    int result = search(nums, target);
+    cout << result << endl;
+
+    return 0;
+}
